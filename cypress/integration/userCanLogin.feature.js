347c0ff8b34f7f3b0f9cb52e0ad12df765d2d03e
sara <saralundkvist86@gmail.com>
2020-09-26 14:06:14 +0200

    creating fixture file login.json to use mocks in the test instead of testing against the live backend. Setting the state in App for renderLoginForm and authenticated to be false by default. The renderLoginForm will be set to true by user clicking on the Login button. Using axois for making call to backenda API, to check the credentials of the user login in, if these are true the state authenticated will be set to true. Tests passes green

--- cypress/integration/userCanLogin.feature.js
+++ cypress/integration/userCanLogin.feature.js
@@ -9,17 +9,17 @@ describe("User authenticates", () => {
       method: "POST",
       url: "http://localhost:3000/api/v1/auth/sign_in",
       response: "fixture:login.json",
-      header: {
+      headers: {
         uid: "user@mail.com",
       },
     });
     cy.get("#login").click();
     cy.get("#login-form").within(() => {
-      cy.get("#email").type("user@email.com");
+      cy.get("#email").type("user@mail.com");
       cy.get("#password").type("password");
       cy.get("button").contains("Submit").click();
     });
-    cy.get("#message").should("contain", "You're logged in as: user@mail.com");
+    cy.get("#message").should("contain", "Hi user@mail.com");
   });
 
   it("with invalid credentials", () => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
 import ProductsList from "./components/ProductsList";
-import NavBar from './components/NavBar'
+import NavBar from "./components/NavBar";
+import LoginForm from "./components/LoginForm";
+import { authenticate } from "./modules/auth";
 
-const App = () => {
-  return (
-    <>
-      <NavBar />
-      <ProductsList />
-      <button id="login">Login</button>
-    </>
-  );
-};
+class App extends Component {
+  state = {
+    renderLoginform: false,
+    authenticated: false,
+    message: "",
+  };
 
+  onLogin = async (e) => {
+    e.preventDefault();
+    const response = await authenticate(
+      e.target.email.value,
+      e.target.password.value
+    );
+    if (response.authenticated) {
+      this.setState({ authenticated: true });
+    } else {
+      this.setState({ message: response.message, renderLoginForm: false });
+    }
+  };
+
+  render() {
+    const { renderLoginForm, authenticated, message } = this.state;
+    let renderLogin;
+    switch (true) {
+      case renderLoginForm && !authenticated:
+        renderLogin = <LoginForm submitFormHandler={this.onLogin} />;
+        break;
+      case !renderLoginForm && !authenticated:
+        renderLogin = (
+          <>
+            <button
+              id="login"
+              onClick={() => this.setState({ renderLoginForm: true })}
+            >
+              Login
+            </button>
+            <p id="message">{message}</p>
+          </>
+        );
+        break;
+      case authenticated:
+        renderLogin = (
+          <p id="message">
+            Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}
+          </p>
+        );
+
+        break;
+      default:
+        break;
+    }
+
+    return (
+      <>
+        {renderLogin}
+        <NavBar />
+        <ProductsList />
+      </>
+    );
+  }
+}
 export default App;
